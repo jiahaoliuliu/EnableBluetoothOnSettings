63d36a6e13dfbddd97aaaf41573f5a774c36bcbb
Jiahao Liu <jiahao.liu@dnata.com>
2019-08-31 14:50:55 +0400

    Updated the view to enable the bluetooth when it is disabled by requesting it to the user

--- app/src/main/java/com/jiahaoliuliu/enablebluetoothonsettings/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/enablebluetoothonsettings/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.jiahaoliuliu.enablebluetoothonsettings
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.lang.Compiler.disable
-import java.lang.Compiler.enable
 import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import androidx.databinding.DataBindingUtil
 import com.jiahaoliuliu.enablebluetoothonsettings.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val REQUEST_CODE_BLUETOOTH_ENABLE = 1000
+    }
+
     private lateinit var mainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mainBinding.bluetoothButton.setOnClickListener{
+            requestForBluetooth(isBluetoothEnabled())
+        }
     }
 
     override fun onResume() {
@@ -23,16 +30,25 @@ class MainActivity : AppCompatActivity() {
         updateView()
     }
 
-    private fun setBluetooth(enable: Boolean): Boolean {
-        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        val isEnabled = isBluetoothEnabled()
-        if (enable && !isEnabled) {
-            return bluetoothAdapter.enable()
-        } else if (!enable && isEnabled) {
-            return bluetoothAdapter.disable()
+    private fun requestForBluetooth(isBluetoothEnabled: Boolean) {
+        // Request to disable bluetooth
+        if (isBluetoothEnabled) {
+            return
         }
-        // No need to change bluetooth state
-        return true
+
+        // Request to enable bluetooth
+        val requestToEnableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        startActivityForResult(requestToEnableBluetoothIntent, REQUEST_CODE_BLUETOOTH_ENABLE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_BLUETOOTH_ENABLE) {
+            if (resultCode == Activity.RESULT_OK) {
+                // The bluetooth has been enabled correctly
+            }
+            updateView()
+        }
     }
 
     private fun isBluetoothEnabled() = BluetoothAdapter.getDefaultAdapter().isEnabled
