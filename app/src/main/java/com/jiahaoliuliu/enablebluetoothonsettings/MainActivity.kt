0eaddd68e116128dfa7663a8a091f93830677cc8
Jiahao Liu <jiahao.liu@dnata.com>
2019-08-31 14:29:44 +0400

    Update the status of the bluetooth button to enable/disable the blueetooth

--- app/src/main/java/com/jiahaoliuliu/enablebluetoothonsettings/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/enablebluetoothonsettings/MainActivity.kt
@@ -2,11 +2,42 @@ package com.jiahaoliuliu.enablebluetoothonsettings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Compiler.disable
+import java.lang.Compiler.enable
+import android.bluetooth.BluetoothAdapter
+import androidx.databinding.DataBindingUtil
+import com.jiahaoliuliu.enablebluetoothonsettings.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateView()
+    }
+
+    private fun setBluetooth(enable: Boolean): Boolean {
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        val isEnabled = isBluetoothEnabled()
+        if (enable && !isEnabled) {
+            return bluetoothAdapter.enable()
+        } else if (!enable && isEnabled) {
+            return bluetoothAdapter.disable()
+        }
+        // No need to change bluetooth state
+        return true
+    }
+
+    private fun isBluetoothEnabled() = BluetoothAdapter.getDefaultAdapter().isEnabled
+
+    private fun updateView() {
+        mainBinding.isBluetoothEnabled = isBluetoothEnabled()
     }
 }
